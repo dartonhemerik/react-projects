09e3eec0528afcdfa677cc105db143a6b635f4e2
Darton Hemerik <73367214+dartonhemerik@users.noreply.github.com>
2021-02-08 15:13:31 -0800

    React Tutorial by John Smilga

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Review from './Review';
+// import { FaGithubSquare } from 'react-icons/fa';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <div className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+        </div>
+        <div className='underline'></div>
+        <Review />
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  console.log(hexColor);
+  const [isAlert, setIsAlert] = useState(false);
+  const bcg = rgb.join(',');
+  // const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
 
-export default SingleColor
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [isAlert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setIsAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {/* <p className="color-value">{hex}</p> */}
+      {isAlert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
